9b2d1ae545647cb0b7b16ed2bd07bc7db526a793
myeee <mye785c83@gmail.com>
2021-03-18 10:29:54 +0900

    move consulting part of myuni

---
+++ src/app/ged-consulting/ged-consulting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ged-consulting',
+  templateUrl: './ged-consulting.component.html',
+  styleUrls: ['./ged-consulting.component.scss']
+})
+export class GedConsultingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
