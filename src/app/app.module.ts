a515fb65c127e5996d291fd99041fa71f72abb5e
myeee <mye785c83@gmail.com>
2021-03-24 19:31:01 +0900

    main background image 배치, 상담신청버튼 위치 수정

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ReviewsComponent } from './reviews/reviews.component';
 import { MainComponent } from './main/main.component';
 import { GedConsultingComponent } from './ged-consulting/ged-consulting.component';
 import { GedConversionComponent } from './ged-conversion/ged-conversion.component';
+import { HeaderComponent } from './main/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    NavBarComponent,
     ReviewsComponent,
     MainComponent,
     GedConsultingComponent,
-    GedConversionComponent
+    GedConversionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
