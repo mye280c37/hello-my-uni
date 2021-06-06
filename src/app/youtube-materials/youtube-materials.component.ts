7616a4a0329f9834cfc45e8da31f7ce8cda9faf4
myeee <mye785c83@gmail.com>
2021-06-07 02:51:43 +0900

    youtube link 연결

--- src/app/youtube-materials/youtube-materials.component.ts
+++ src/app/youtube-materials/youtube-materials.component.ts
@@ -1,4 +1,7 @@
+import * as $ from 'jquery';
 import { Component, OnInit } from '@angular/core';
+import ClickEvent = JQuery.ClickEvent;
+
 
 @Component({
   selector: 'app-youtube-materials',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./youtube-materials.component.scss']
 })
 export class YoutubeMaterialsComponent implements OnInit {
+  materialWidth = 720;
+  materialHeight = 405;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeMaterialState(targetId: string): void{
+    const materialEle = $('#' + 'material' + targetId);
+    const titleEle = $('#' + targetId);
+    if (materialEle.hasClass('hidden')) {
+      materialEle.removeClass('hidden');
+      titleEle.addClass('font-weight-bold');
+    }else{
+      materialEle.addClass('hidden');
+      if (titleEle.hasClass('font-weight-bold')) {
+        titleEle.removeClass('font-weight-bold');
+      }
+    }
+  }
 }
